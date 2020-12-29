2ca27e8df87448ab889f516b3513b7f835f0d6db
SwayInCollaborate <sway.chen@collaborate.tw>
2020-12-29 15:46:59 +0800

    加入專案檔案。

---
+++ MovieOrganizer/Handlers/FilmHandler.cs
@@ -0,0 +1,67 @@
+﻿using MovieOrganizer.Abstracts;
+using MovieOrganizer.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieOrganizer.Handlers
+{
+    public class FilmHandler : DirRecursiveHandler
+    {
+        public IList<string> EmptyFileDirs { get; } = new List<string>();
+        public IList<FilmModel> Films { get; } = new List<FilmModel>();
+
+        protected override void ProcessFiles(string path, IEnumerable<string> files)
+        {
+            if (!files.Any())
+            {
+                EmptyFileDirs.Add(path);
+                return;
+            }
+
+            foreach (var file in files)
+            {
+                var model = ExtractFilmInfo(file);
+                Films.Add(model);
+            }            
+        }
+
+        private FilmModel ExtractFilmInfo(string file)
+        {
+            var model = new FilmModel(file);
+            var brackets = ExtractBrackets(model.FileName);
+            var counts = brackets.Count();
+            if (counts >= 2)
+            {
+                model.Distributor = brackets.ElementAt(0);
+                model.Identification = brackets.ElementAt(1);
+            }
+
+            if (counts == 1)
+            {
+                var content = brackets.ElementAt(0);
+                if (content.Contains("-"))
+                    model.Identification = content;
+                else
+                    model.Distributor = content;
+            }
+
+            return model;
+        }
+
+        private IEnumerable<string> ExtractBrackets(string fileName)
+        {
+            if (!fileName.Contains("(") || !fileName.Contains(")"))
+                yield break;
+
+            var leftBracket = fileName.IndexOf("(");
+            var rightBracket = fileName.IndexOf(")");
+            if (leftBracket < 0 || rightBracket < 0)
+                yield break;
+
+            var innerText = fileName.Substring(leftBracket + 1, rightBracket - leftBracket + 1);
+            yield return innerText;
+            var nextFileName = fileName.Substring(rightBracket);
+            ExtractBrackets(nextFileName);
+        }
+    }
+}
---
+++ MovieOrganizer/Program.cs
@@ -0,0 +1,30 @@
+﻿using MovieOrganizer.Adaptors;
+using System;
+
+namespace MovieOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+        }
+
+        private static void Execute()
+        {
+            Console.WriteLine("Enter Path");
+            var command = Console.ReadLine();
+            if (string.Equals(command, "exit", StringComparison.InvariantCultureIgnoreCase))
+                return;
+
+            Console.WriteLine("Start Process");
+            Console.WriteLine("============================================");
+            var adaptor = new FilmAdaptor();
+            adaptor.Categorize(command);
+            Console.WriteLine("============================================");
+            Console.WriteLine("End Process");
+            Console.WriteLine("");
+            Execute();
+        }
+    }
+}
