ad4d10e51194d7d8d9b58c2193e439fdde1b80dc
SwayChen <sway.chen@collaborate.tw>
2021-02-01 11:56:16 +0800

    建立物件備註

--- Category.Standard/Models/DistributorCat.cs
+++ Category.Standard/Models/DistributorCat.cs
@@ -1,5 +1,8 @@
 ﻿namespace Category.Standard.Models
 {
+    /// <summary>
+    /// 發行商-目錄
+    /// </summary>
     public class DistributorCat
     {
         public string Distributor { get; set; }
