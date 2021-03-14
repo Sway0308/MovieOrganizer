8cf55957960ae242f442e572331344ccfae7fa70
Sway0308 <sway0308@gmail.com>
2021-03-14 22:21:43 +0800

    複寫Equals

--- Category.Standard/Models/DistributorCat.cs
+++ Category.Standard/Models/DistributorCat.cs
@@ -1,4 +1,6 @@
-﻿namespace Category.Standard.Models
+﻿using Gatchan.Base.Standard.Base;
+
+namespace Category.Standard.Models
 {
     /// <summary>
     /// 發行商-目錄
@@ -7,5 +9,13 @@
     {
         public string Distributor { get; set; }
         public string Category { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var dis = obj as DistributorCat;
+            if (Distributor == null)
+                return false;
+            return dis.Distributor.SameText(Distributor) && dis.Category.SameText(Category);
+        }
     }
 }
